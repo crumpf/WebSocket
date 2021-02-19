fb41f3a32cb4ecdb340381ba0cb3398c679c0ba8
Chris Rumpf <crumpf@crumpf.com>
2021-02-18 23:52:50 -0500

    Initial Commit

---
+++ WebSocket/WebSocket.h
@@ -0,0 +1,18 @@
+//
+//  WebSocket.h
+//  WebSocket
+//
+//  Created by Christopher Rumpf on 2/18/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WebSocket.
+FOUNDATION_EXPORT double WebSocketVersionNumber;
+
+//! Project version string for WebSocket.
+FOUNDATION_EXPORT const unsigned char WebSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebSocket/PublicHeader.h>
+
+
